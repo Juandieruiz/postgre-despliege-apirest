ad3c5347eae94ca8ceb17ba1ae3a750c6a9f2a28
juand <juand@LAPTOP-1PSV7CB6.mshome.net>
2021-08-02 13:22:29 +0200

    1

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,3 +4,4 @@ insert into cars (fabricante, model,cc,doors,available,year,release_date) VALUES
 insert into cars (fabricante, model,cc,doors,available,year,release_date) VALUES ('Coche2', 'Coche2', 2.1, 5, true, 1999, '2020-12-30');
 insert into cars (fabricante, model,cc,doors,available,year,release_date) VALUES ('Coche3', 'Coche3', 2.1, 5, true, 1999, '2020-12-30');
 insert into cars (fabricante, model,cc,doors,available,year,release_date) VALUES ('Coche4', 'Coche4', 2.1, 5, true, 1999, '2020-12-30');
+insert into app_users(username, email, password) values ('juan', 'juan@', '$2a$10$qINz45KQzkLHtzOovvgjn.y1SKh8zfR5Meyh61FHz3bvSlO4uez.O');
